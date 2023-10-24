fab2365598a8f49ddd30d938d44cbe4ea2d286a7
Anthony <gachinigaithuma@gmail.com>
2023-10-24 10:52:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+
+} 
+var bestCustomer;
+function setBestCustomer(){
+   bestCustomer = 'not bob';
+    
+   
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+const leastFavoriteCustomer = 'Vic';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Anthony';
+
+}
+
+
+
+
